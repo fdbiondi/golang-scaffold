820dbea4be3338ae61d21e3eb364999ebc7a8712
fbiondi <federico.biondi@rms.com>
2023-09-23 17:05:21 -0300

    first commit

---
+++ cmd/golang-scaffold/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/fdbiondi/golang-scaffold/internal/sample"
+
+func main() {
+	sample.HelloWorld()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fdbiondi/golang-scaffold
+
+go 1.21.0
