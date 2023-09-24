93c9468d92165f4ddf608b8151d8f5cad81a9f14
fbiondi <federico.biondi@rms.com>
2023-09-24 11:42:35 -0300

    refactor

--- internal/scaffold/scaffold.go
+++ internal/scaffold/scaffold.go
@@ -17,12 +17,12 @@ type Project struct {
 	dir       string
 	repoOwner string
 	name      string
+	modName   string
 }
 
 func CreateProject() {
-	var project, err = getUserInput()
+	var project, err = getProjectInfo()
 	if err != nil {
-		// fmt.Println(err)
 		log.Fatal(err)
 		os.Exit(1)
 	}
@@ -40,17 +40,35 @@ func CreateProject() {
 		log.Fatal(err)
 		os.Exit(1)
 	}
-
 }
 
-func getUserInput() (Project, error) {
+func getProjectDir() (string, error) {
 	flag.Parse()
 
 	projectDir := flag.Arg(0)
 	if projectDir == "" {
 		projectDir = DEFAULT_DIR
-	} else if _, err := os.Stat("./" + projectDir); !os.IsNotExist(err) {
-		return Project{}, fmt.Errorf("directory already exists: '%s'", projectDir)
+
+	} else if _, err := os.Stat(projectDir); !os.IsNotExist(err) {
+		return "", fmt.Errorf("a directory named '%s' already exists", projectDir)
+	}
+
+	if _, err := os.Stat(projectDir + "/go.mod"); !os.IsNotExist(err) {
+		pwd, err := os.Getwd()
+		if err != nil {
+			pwd = projectDir
+		}
+
+		return "", fmt.Errorf("go module already exists on '%s'", pwd)
+	}
+
+	return projectDir, nil
+}
+
+func getProjectInfo() (Project, error) {
+	projectDir, err := getProjectDir()
+	if err != nil {
+		return Project{}, err
 	}
 
 	var repoOwner = ""
@@ -66,10 +84,13 @@ func getUserInput() (Project, error) {
 		fmt.Scanln(&projectName)
 	}
 
+	modName := fmt.Sprintf("%s/%s/%s", DEFAULT_REMOTE, repoOwner, projectName)
+
 	return Project{
 		dir:       projectDir,
 		repoOwner: repoOwner,
 		name:      projectName,
+		modName:   modName,
 	}, nil
 }
 
@@ -89,42 +110,43 @@ func getOutputDir(project Project) (string, error) {
 }
 
 func createStructure(project Project, outputDir string) error {
-
 	fmt.Println("Creating project directory...")
 	if project.dir != DEFAULT_DIR {
-		cmd := exec.Command("mkdir", "-p", project.dir)
-		if err := cmd.Run(); err != nil {
+		if err := os.MkdirAll(project.dir, 0755); err != nil {
 			return err
 		}
 	}
 
 	fmt.Println("Initializing project...")
-	cmd := exec.Command("go", "mod", "init", fmt.Sprintf("%s/%s/%s", DEFAULT_REMOTE, project.repoOwner, project.name))
+	cmd := exec.Command("go", "mod", "init", project.modName)
 	cmd.Dir = outputDir
 	if err := cmd.Run(); err != nil {
 		return err
 	}
 
-	fmt.Println("Directories scaffolding...")
-	cmd = exec.Command("mkdir", "-p", "bin", "src", "internal/sample", "tests", fmt.Sprintf("src/%s", project.name))
-	cmd.Dir = outputDir
-	if err := cmd.Run(); err != nil {
-		return err
+	var internalModule = "sample"
+	var dirs = []string{
+		project.dir + "/bin",
+		project.dir + "/internal/" + internalModule,
+		project.dir + "/src/" + project.name,
+		project.dir + "/tests",
+	}
+
+	for _, dir := range dirs {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			return err
+		}
 	}
 
 	fmt.Println("Creating main.go file...")
-	cmd = exec.Command("touch", fmt.Sprintf("src/%s/main.go", project.name))
-	cmd.Dir = outputDir
-	if err := cmd.Run(); err != nil {
+	_, err := os.Create(fmt.Sprintf("%s/src/%s/main.go", project.dir, project.name))
+	if err != nil {
 		return err
 	}
 
-	var module = "sample"
-
-	fmt.Printf("Creating %s module...\n", module)
-	cmd = exec.Command("touch", fmt.Sprintf("internal/%s/%s.go", module, module))
-	cmd.Dir = outputDir
-	if err := cmd.Run(); err != nil {
+	fmt.Printf("Creating %s module...\n", internalModule)
+	_, err = os.Create(fmt.Sprintf("%[1]s/internal/%[2]s/%[2]s.go", project.dir, internalModule))
+	if err != nil {
 		return err
 	}
 
